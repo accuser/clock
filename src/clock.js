a6644408c988408b0e52c3e00c0aeec695807590
Matthew Gibbons <matthew.gibbons@multiverse.io>
2022-11-14 09:34:28 +0000

    Tabs, not spaces

--- src/clock.js
+++ src/clock.js
@@ -1,5 +1,5 @@
 /**
- * @param {HTMLTimeElement} clock - the target <time> element 
+ * @param {HTMLTimeElement} clock - the target <time> element
  * @param {HTMLTemplateElement} template - dot matrix template
  */
 const start = (clock, template = clock.children[0]) => {
@@ -10,24 +10,24 @@ const start = (clock, template = clock.children[0]) => {
 	template.remove();
 
 	/**
-   * @param {Date} datetime - the datetime to render
-   */
+	 * @param {Date} datetime - the datetime to render
+	 */
 	const tick = (datetime = new Date()) => {
-		clock.setAttribute('datetime', datetime.toISOString());
+		clock.setAttribute("datetime", datetime.toISOString());
 
-		const separator = datetime.getMilliseconds() < 500 ? ':' : ' ';
+		const separator = datetime.getMilliseconds() < 500 ? ":" : " ";
 
 		const digits = [
 			datetime.getHours(),
 			datetime.getMinutes(),
-			datetime.getSeconds()
+			datetime.getSeconds(),
 		]
-			.map((value) => value.toString().padStart(2, '0'))
+			.map((value) => value.toString().padStart(2, "0"))
 			.join(separator);
 
 		for (let i = 0; i < 8; i++) {
-			clock.children.item(i).setAttribute('data', digits[i]);
-		} 
+			clock.children.item(i).setAttribute("data", digits[i]);
+		}
 	};
 
 	setInterval(tick, 100);
